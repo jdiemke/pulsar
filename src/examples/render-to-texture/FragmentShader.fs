8294f2ac816ee5f17cb4631fa175dd497e5d802c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-01 18:44:31 +0000

    Deploy jdiemke/pulsar to github.com/jdiemke/pulsar.git:gh-pages

--- src/examples/render-to-texture/FragmentShader.fs
+++ src/examples/render-to-texture/FragmentShader.fs
@@ -12,11 +12,11 @@ void main() {
 
     float color1, color2, color;
 	
-	color1 = (sin(dot(gl_FragCoord.xy,vec2(sin(time*3.0),cos(time*3.0)))*0.02+time*3.0)+1.0)/2.0;
+	color1 = (sin(dot(gl_FragCoord.xy*5.0,vec2(sin(time*3.0),cos(time*3.0)))*0.02+time*3.0)+1.0)/2.0;
 	
 	vec2 center = vec2(640.0/2.0, 360.0/2.0) + vec2(640.0/2.0*sin(-time*3.0),360.0/2.0*cos(-time*3.0));
 	
-	color2 = (cos(length(gl_FragCoord.xy - center)*0.03)+1.0)/2.0;
+	color2 = (cos(length(gl_FragCoord.xy*5.0 - center)*0.03)+1.0)/2.0;
 	
 	color = (color1+ color2)/2.0;
 
--- src/shader-programs/texture-mapping-lighting/SphereMapping.frag
+++ src/shader-programs/texture-mapping-lighting/SphereMapping.frag
@@ -4,9 +4,10 @@ precision mediump float;
 
 uniform sampler2D utexture;
 in vec2 tex;
+in vec3 color;
 out vec4 outColor;
 
 void main() {
-    outColor =texture(utexture, tex);
+    outColor =texture(utexture, tex) * vec4(color, 1.0);
 }
 
